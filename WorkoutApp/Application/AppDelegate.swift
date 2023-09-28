69d528ebad43abb9fa50d644651d69a2884da1e3
Ilya Rodin <i1.rodin@yandex.ru>
2023-09-28 04:55:46 +0300

    add tab bar with icons

---
+++ WorkoutApp/Application/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  WorkoutApp
+//
+//  Created by Илья Родин on 9/28/23.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
